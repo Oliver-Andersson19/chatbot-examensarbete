28081ae2635ded4e0754ed4a84d0bc5889356758
Oliver Andersson <oliver.andersson101@gmail.com>
2024-05-02 23:19:38 +0200

    added companyname to chatbot

--- backend/src/controllers/chatbotController.js
+++ backend/src/controllers/chatbotController.js
@@ -36,10 +36,11 @@ async function runQuery(req, res) {
 
 
 async function createChatbot(req, res) {
-    const username = req.decoded.username
-    
+    const username = req.decoded.username;
     const { chatbotData } = req.body;
-    const faq = chatbotData.faq
+    const faq = chatbotData.faq;
+    const companyName = chatbotData.companyName;
+
     const colorScheme = {
         primaryColor: chatbotData.primaryColor,
         secondaryColor: chatbotData.secondaryColor,
@@ -55,7 +56,7 @@ async function createChatbot(req, res) {
         const userInfo = await userModel.getUser(username)
         const userId = userInfo[0].id
 
-        const chatbotId = await chatbotModel.createChatbot(faq, colorScheme, userId);
+        const chatbotId = await chatbotModel.createChatbot(faq, colorScheme, companyName, userId);
   
         res.status(200).json(chatbotId);
       
@@ -65,5 +66,26 @@ async function createChatbot(req, res) {
     }
 }
 
+async function getChatbotList(req, res) {
+    const username = req.decoded.username
+    
+    if (username == undefined)
+        return res.status(500).json({ error: "no email or account provided" });
 
-export default { serveJS, runQuery, createChatbot }
+    try {
+
+        const userInfo = await userModel.getUser(username)
+        const userId = userInfo[0].id
+
+        const chatbots = await chatbotModel.getChatbotsByUserId(userId);
+  
+        res.status(200).json(chatbots);
+      
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+}
+
+
+export default { serveJS, runQuery, createChatbot, getChatbotList }
