5ed01e4d1f5ad7235089bad66f4d08c768d676f1
Oliver Andersson <oliver.andersson101@gmail.com>
2024-05-10 03:02:56 +0200

    rag template removed

--- backend/src/models/ollamaModel.js
+++ backend/src/models/ollamaModel.js
@@ -24,11 +24,10 @@ async function createChainForId(id) {
     const chatbotInformation = await chatbotModel.getChatbotInformation(id)
     const url = chatbotInformation[0].FAQ
 
-    console.log(url)
-
-        
-    const loader = new CheerioWebBaseLoader(url);
+   
+    const loader = new CheerioWebBaseLoader("https://www.visimedia.se/faq");
     const data = await loader.load();
+
 
     const textSplitter = new RecursiveCharacterTextSplitter({
         chunkSize: 500,
@@ -37,26 +36,26 @@ async function createChainForId(id) {
 
     const allSplits = await textSplitter.splitDocuments(data);
 
-
+    console.log("OllamaEmbed start")
     const embeddings = new OllamaEmbeddings();
+    
+    console.log("OllamaEmbed end, Vector start")
     const vectorStore = await MemoryVectorStore.fromDocuments(
         allSplits,
         embeddings
     );
+    console.log("Vector End")
 
     const promptTemplate = `
     You are a helpful chatbot implemented on a Website. Limit your answers to a maximum of 2 sentences.
     If needed use the content of the website provided in the context to answer the humans question. Do not mention the provided context.
     Do not use the content of the context if it is not relevant to the question.
     If you cant find an answer in the context do not try to make up an answer simply say that you could not find an answer to the humans question.
-    \n\nContext: {context}\n\nQuestion: {question}\nHelpful Answer:`;
-    const qaPrompt = PromptTemplate.fromTemplate(promptTemplate);
-
+    \n\nContext: {context}\n\nQuestion: {question}\nHelpful Answer:`
+    // const qaPrompt = PromptTemplate.fromTemplate(promptTemplate);
+    console.log("returning qa chain")
     const retriever = vectorStore.asRetriever();
-    return new RetrievalQAChain({
-        combineDocumentsChain: loadQAStuffChain(ollama, {prompt: qaPrompt}),
-        retriever: retriever
-    });
+    return RetrievalQAChain.fromLLM(ollama, retriever);
 }
 
 
@@ -72,6 +71,7 @@ async function prompt(query, id) {
     if (!chainList[id]) {
         // If not, create a new chain and store it in the map
         chain = await createChainForId(id);
+        console.log("qa chain returned")
         chainList[id] = chain;
     } else {
         // If exists, use the existing chain from the map
@@ -79,7 +79,11 @@ async function prompt(query, id) {
     }
 
     try {
+        
+        console.log("prompt called")
         const result = await chain.call({query: query});
+        
+        console.log("prompt done")
         return result.text; // Return the text response
     } catch (error) {
         console.error("Error executing query:", error);
